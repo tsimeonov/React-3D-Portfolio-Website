5971dc0e7f7b2c906b2f5eac9cda06f13ec59438
tsimeonov <tsimeonov12@gmail.com>
2024-06-01 13:12:28 +0200

    buiding the component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.div``;
+const Container = styled.div``;
+
+export const Navbar = () => {
+  return <Section>Navbar</Section>;
+};
+
+export default Navbar;
