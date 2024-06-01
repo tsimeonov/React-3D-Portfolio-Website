b704124354fd8b1e02e4298b671cf162ed10bff0
tsimeonov <tsimeonov12@gmail.com>
2024-06-01 13:14:27 +0200

    buiding the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-const Section = styled.div``;
-const Container = styled.div``;
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 1400px;
+  background-color: rebeccapurple;
+`;
 
 export const Navbar = () => {
-  return <Section>Navbar</Section>;
+  return (
+    <Section>
+      <Container>Navbar</Container>
+    </Section>
+  );
 };
 
 export default Navbar;
