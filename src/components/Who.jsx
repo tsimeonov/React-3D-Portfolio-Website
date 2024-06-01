cee47a2ace88002f6caf16eac60c1d5e10d2afff
tsimeonov <tsimeonov12@gmail.com>
2024-06-01 15:16:52 +0200

    styling the who component

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,13 +1,88 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Section = styled.section`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled.section`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
 `;
 
 const Who = () => {
-  return <Section>Who</Section>;
+  return (
+    <Section>
+      <Container>
+        <Left>{/* 3D Model */}</Left>
+        <Right>
+          <Title>Think. Make. Solve</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>What we do</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            we wnjoy creating delightful, human-centered digital experiences.
+          </Desc>
+          <Button>Learn More</Button>
+        </Right>
+      </Container>
+    </Section>
+  );
 };
 
 export default Who;
