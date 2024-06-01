07f5cd2078f19f480b2837fbf8a3caf765be5da0
tsimeonov <tsimeonov12@gmail.com>
2024-06-01 14:19:04 +0200

    styling the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,26 @@ import { Navbar } from "./Navbar";
 const Section = styled.section`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Container = styled.section`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Hero = () => {
   return (
     <Section>
       <Navbar />
+      <Container>Hero</Container>
     </Section>
   );
 };
