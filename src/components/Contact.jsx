a3ebe78cb6fdaefb73c397fbac356b9452a928cc
tsimeonov <tsimeonov12@gmail.com>
2024-06-02 13:59:50 +0200

    styling the contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,30 @@ const Section = styled.section`
   scroll-snap-align: center;
 `;
 
+const Container = styled.section`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+
+const Left = styled.section`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+
+const Title = styled.section`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+
+const Input = styled.section`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+
+const Section = styled.section`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
 const Contact = () => {
   return <Section>Contact</Section>;
 };
