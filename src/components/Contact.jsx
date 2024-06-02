31b25be7ed7f331f3b2bd20e3e994f00a50dc79e
tsimeonov <tsimeonov12@gmail.com>
2024-06-02 14:16:26 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,50 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
-const Section = styled.section`
-  height: 100vh;
-  scroll-snap-align: center;
-`;
+const Section = styled.section``;
 
-const Container = styled.section`
-  height: 100vh;
-  scroll-snap-align: center;
-`;
+const Container = styled.section``;
 
-const Left = styled.section`
-  height: 100vh;
-  scroll-snap-align: center;
-`;
+const Left = styled.section``;
 
-const Title = styled.h1`
-  height: 100vh;
-  scroll-snap-align: center;
-`;
+const Title = styled.h1``;
 
-const Form = styled.form`
-  height: 100vh;
-  scroll-snap-align: center;
-`;
+const Form = styled.form``;
 
-const Input = styled.input`
-  height: 100vh;
-  scroll-snap-align: center;
-`;
+const Input = styled.input``;
 
-const TextArea = styled.textarea`
-  height: 100vh;
-  scroll-snap-align: center;
-`;
+const TextArea = styled.textarea``;
 
-const Button = styled.button`
-  height: 100vh;
-  scroll-snap-align: center;
-`;
+const Button = styled.button``;
 
-const Right = styled.section`
-  height: 100vh;
-  scroll-snap-align: center;
-`;
+const Right = styled.section``;
 const Contact = () => {
   return (
     <Section>
