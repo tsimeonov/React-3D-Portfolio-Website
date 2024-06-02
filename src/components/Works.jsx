36e9c29703aee4b5d3033a261abd4d81b40b175e
tsimeonov <tsimeonov12@gmail.com>
2024-06-02 13:51:57 +0200

    refactor

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -36,10 +36,11 @@ const List = styled.ul`
 `;
 
 const ListItem = styled.li`
-  font-size: 100px;
+  font-size: 90px;
   font-weight: bold;
   cursor: pointer;
   color: transparent;
+  white-space: nowrap;
   -webkit-text-stroke: 1px white;
   position: relative;
 
