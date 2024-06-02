1e126a6a3ea0e05fb677b250a1ddff93d2b8dc2c
tsimeonov <tsimeonov12@gmail.com>
2024-06-02 12:53:53 +0200

    add the data variable

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
+const data = [
+  "Web Design",
+  "Development",
+  "Illustration",
+  "Product Design",
+  "Social Media",
+];
+
 const Section = styled.section`
   height: 100vh;
   scroll-snap-align: center;
