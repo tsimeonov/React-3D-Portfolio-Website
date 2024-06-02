60e8af0363af1c188e3105db3a9d58d256474457
tsimeonov <tsimeonov12@gmail.com>
2024-06-02 14:17:37 +0200

    style the section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
-const Section = styled.section``;
+const Section = styled.section`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
 
 const Container = styled.section``;
 
