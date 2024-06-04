968c1154f720d1cebe912d6d5916535a639d18c5
tsimeonov <tsimeonov12@gmail.com>
2024-06-04 18:31:22 +0200

    add another annotation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -14,7 +14,7 @@ const Map = () => {
       projectionConfig={{
         rotate: [-10.0, -52.0, 0],
         center: [-5, -3],
-        scale: 1100,
+        scale: 1600,
       }}
       style={{ width: "100%", height: "100%" }}
     >
@@ -42,6 +42,20 @@ const Map = () => {
       >
         <text x="-8" textAnchor="end" alignmentBaseline="middle" fill="white">
           {"Paris"}
+        </text>
+      </Annotation>
+      <Annotation
+        subject={[21.01178, 52.22977]}
+        dx={-90}
+        dy={-30}
+        connectorProps={{
+          stroke: "white",
+          strokeWidth: 2,
+          strokeLinecap: "round",
+        }}
+      >
+        <text x="-8" textAnchor="end" alignmentBaseline="middle" fill="white">
+          {"Warsaw"}
         </text>
       </Annotation>
     </ComposableMap>
