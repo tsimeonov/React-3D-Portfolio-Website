9494f082c85b640abd6769497a23b926183b504b
tsimeonov <tsimeonov12@gmail.com>
2024-06-07 19:08:54 +0200

    create the text in the cube

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,4 +1,9 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  RenderTexture,
+  Text,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import styled from "styled-components";
@@ -17,8 +22,16 @@ const Test = () => {
         <ambientLight intensity={1} />
         <directionalLight position={[3, 2, 1]} />
         <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="red" />
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial>
+            <RenderTexture attach="map">
+              <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+              <color attach="background" args={["pink"]} />
+              <Text fontSize={0.5} color="#555">
+                hello
+              </Text>
+            </RenderTexture>
+          </meshStandardMaterial>
         </mesh>
       </Canvas>
     </Container>
