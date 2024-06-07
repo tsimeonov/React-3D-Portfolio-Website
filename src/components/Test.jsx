8f21c3a8d312d45b3d883aca2d35d9012b448ede
tsimeonov <tsimeonov12@gmail.com>
2024-06-07 18:42:41 +0200

    add the Canvas

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -4,10 +4,19 @@ import styled from "styled-components";
 const Container = styled.div`
   height: 100%;
   width: 100%;
+  scroll-snap-align: center;
 `;
 
 export const Test = () => {
-  return <div>Test</div>;
+  return;
+
+  <Container>
+    <Canvas>
+      <mesh>
+        <boxGeometry args={[5, 5, 5]} />
+      </mesh>
+    </Canvas>
+  </Container>;
 };
 
 export default Test;
