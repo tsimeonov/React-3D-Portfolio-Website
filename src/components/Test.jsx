60e32b8ca013cd837fb881a4974a80671cd92a5b
tsimeonov <tsimeonov12@gmail.com>
2024-06-07 18:38:35 +0200

    create the container

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
 export const Test = () => {
   return <div>Test</div>;
 };
