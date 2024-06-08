a7834947e85ed36df1a813cd8fbba26b2047d603
tsimeonov <tsimeonov12@gmail.com>
2024-06-08 15:13:21 +0200

    Update Cube.jsx

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -16,7 +16,7 @@ const Cube = () => {
         <RenderTexture attach="map">
           <PerspectiveCamera makeDefault position={[0, 0, 5]} />
           <color attach="background" args={["#dc9dcd"]} />
-          <Text ref={textRef} fontSize={3} color="#555">
+          <Text ref={textRef} fontSize={1} color="#555">
             hello
           </Text>
         </RenderTexture>
