dcfdb98123ba43e22792b5d986c2c305cf30b738
tsimeonov <tsimeonov12@gmail.com>
2024-06-08 14:43:33 +0200

    making the text move

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import {
-  OrbitControls,
-  PerspectiveCamera,
-  RenderTexture,
-  Text,
-} from "@react-three/drei";
+import { useState, useEffect, useRef } from "react";
+import { PerspectiveCamera, RenderTexture, Text } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 
 const Cube = () => {
-  useFrame((state) => console.log(state));
+  const textRef = useRef();
+  useFrame(
+    (state) => (textRef.current.position.x = Math.sin(state.clock.elapsedTime))
+  );
   return (
     <mesh>
       <boxGeometry args={[2, 2, 2]} />
@@ -16,7 +15,7 @@ const Cube = () => {
         <RenderTexture attach="map">
           <PerspectiveCamera makeDefault position={[0, 0, 2]} />
           <color attach="background" args={["#dc9dcd"]} />
-          <Text fontSize={0.5} color="#555">
+          <Text ref={textRef} fontSize={0.5} color="#555">
             hello
           </Text>
         </RenderTexture>
