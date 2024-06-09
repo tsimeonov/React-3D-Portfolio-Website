ee3ca992127947b547ec5c4c6e9b4f73c2ce5273
tsimeonov <tsimeonov12@gmail.com>
2024-06-09 14:13:13 +0200

    style the desc

--- src/components/Development.jsx
+++ src/components/Development.jsx
@@ -13,6 +13,14 @@ const Desc = styled.div`
   position: absolute;
   bottom: 100px;
   right: 100px;
+
+  @media only screen and (max-width: 768px) {
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
 `;
 
 const Development = () => {
