615ebb60ba2796bbb88df870bfc7c543af25b9cc
tsimeonov <tsimeonov12@gmail.com>
2024-06-09 13:23:42 +0200

    add the shoe

--- src/components/Development.jsx
+++ src/components/Development.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stage } from "@react-three/drei";
+import Shoe from "./Shoe";
 
 const Development = () => {
-  return <div>Development</div>;
+  return (
+    <Canvas>
+      <Stage environment="city" intensity={0.6}>
+        <Shoe />
+        {/* <Chair /> */}
+      </Stage>
+      <OrbitControls enableZoom={false} />
+    </Canvas>
+  );
 };
 
 export default Development;
