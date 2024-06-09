8c403f6de6aec9cac272ac540e56f1e264e7556f
tsimeonov <tsimeonov12@gmail.com>
2024-06-09 14:00:37 +0200

    add the width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,6 +31,7 @@ const Container = styled.section`
   justify-content: space-between;
 
   @media only screen and (max-width: 768px) {
+    width: 100%;
     flex-direction: column;
     align-items: center;
     justify-content: center;
