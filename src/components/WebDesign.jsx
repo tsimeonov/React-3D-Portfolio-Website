7a674c6de7cce0196bf0bcf5afccc918adeb925d
tsimeonov <tsimeonov12@gmail.com>
2024-06-09 13:21:55 +0200

    import

--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -2,12 +2,14 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stage } from "@react-three/drei";
 import React from "react";
 import Chair from "./Chair";
+import Mac from "./Mac";
 
 const WebDesign = () => {
   return (
     <Canvas>
       <Stage environment="city" intensity={0.6}>
-        <Chair />
+        <Mac />
+        {/* <Chair /> */}
       </Stage>
       <OrbitControls enableZoom={false} />
     </Canvas>
