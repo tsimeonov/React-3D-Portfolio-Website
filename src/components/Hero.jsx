5c2ebfb05ecd3a36f5ac5968d6d5fe0ee351186e
tsimeonov <tsimeonov12@gmail.com>
2024-06-09 14:05:44 +0200

    style the img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -116,6 +116,11 @@ const Img = styled.img`
   margin: auto;
   animation: animate 2s infinite ease alternate;
 
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+    height: 300px;
+  }
+
   @keyframes animate {
     to {
       transform: translateY(20px);
