da9dcfb8c7e6d56db1395636f8ce65bb1a997ee7
tsimeonov <tsimeonov12@gmail.com>
2024-06-09 13:28:20 +0200

    Update WebDesign.jsx

--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -3,16 +3,25 @@ import { OrbitControls, Stage } from "@react-three/drei";
 import React from "react";
 import Chair from "./Chair";
 import Mac from "./Mac";
+import styled from "styled-components";
+
+const Desc = styled.div``;
 
 const WebDesign = () => {
   return (
-    <Canvas>
-      <Stage environment="city" intensity={0.6}>
-        <Mac />
-        {/* <Chair /> */}
-      </Stage>
-      <OrbitControls enableZoom={false} />
-    </Canvas>
+    <>
+      <Canvas>
+        <Stage environment="city" intensity={0.6}>
+          <Mac />
+          {/* <Chair /> */}
+        </Stage>
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+      <Desc>
+        We design products with a strong focus on both world cladd design and
+        ensuring your product is a success.
+      </Desc>
+    </>
   );
 };
 
