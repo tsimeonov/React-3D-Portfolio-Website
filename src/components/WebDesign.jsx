8591cc52861313d566dbee12a2b8621efd8ba2c1
tsimeonov <tsimeonov12@gmail.com>
2024-06-09 13:16:43 +0200

    add the chair model

--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -1,7 +1,17 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stage } from "@react-three/drei";
 import React from "react";
+import Chair from "./Chair";
 
 const WebDesign = () => {
-  return <div>WebDesign</div>;
+  return (
+    <Canvas>
+      <Stage environment="city" intensity={0.6}>
+        <Chair />
+      </Stage>
+      <OrbitControls enableZoom={false} />
+    </Canvas>
+  );
 };
 
 export default WebDesign;
