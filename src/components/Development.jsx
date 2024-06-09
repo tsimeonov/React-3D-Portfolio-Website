1d197d096aca397e30c010445c214bb42cfc2928
tsimeonov <tsimeonov12@gmail.com>
2024-06-09 13:34:47 +0200

    refactor

--- src/components/Development.jsx
+++ src/components/Development.jsx
@@ -2,16 +2,34 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stage } from "@react-three/drei";
 import Shoe from "./Shoe";
+import styled from "styled-components";
+
+const Desc = styled.div`
+  width: 200px;
+  height: 70px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 10px;
+  position: absolute;
+  bottom: 100px;
+  right: 100px;
+`;
 
 const Development = () => {
   return (
-    <Canvas>
-      <Stage environment="city" intensity={0.6}>
-        <Shoe />
-        {/* <Chair /> */}
-      </Stage>
-      <OrbitControls enableZoom={false} />
-    </Canvas>
+    <>
+      <Canvas>
+        <Stage environment="city" intensity={0.6}>
+          <Shoe />
+          {/* <Chair /> */}
+        </Stage>
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+      <Desc>
+        We design products with a strong focus on both world cladd design and
+        ensuring your product is a success.
+      </Desc>
+    </>
   );
 };
 
